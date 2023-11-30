860a62d780a431d44a7f5789b6d29d4bfda6f196
Joaomonsy <joao.lima548@etec.sp.gov.br>
2023-11-30 08:23:21 -0300

    RoomFinal

---
+++ app/src/main/java/com/example/sqliteroom/model/Contato.kt
@@ -0,0 +1,13 @@
+package com.example.sqliteroom.model
+
+import com.example.sqliteroom.controller.Contato
+import com.example.sqliteroom.controller.Tipos
+
+data class Contato(
+    val contato: List<Contato> = emptyList(),
+    val nome: String = "",
+    val sobrenome: String = "",
+    val telefone: String = "",
+    val adicionarContato: Boolean = false,
+    val tipos: Tipos = Tipos.nome
+)
